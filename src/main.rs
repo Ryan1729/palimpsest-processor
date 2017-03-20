4a63071c3585650f0a417f0111b07cb411090ba6
Ryan1729 <Ryan1729@gmail.com>
2017-03-19 18:34:30 -0600

    get some basic hex printing working
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,23 @@ extern crate bear_lib_terminal;
 
 use bear_lib_terminal::Color;
 use bear_lib_terminal::geometry::Point;
-use bear_lib_terminal::terminal::{self, config, Event, KeyCode};
+use bear_lib_terminal::terminal::{self, config, Event, KeyCode, state};
 
 
 fn main() {
-    terminal::open("Simple example", 80, 30);
+    terminal::open("DUEL17", 80, 30);
     terminal::set(config::Window::empty().resizeable(true));
 
-    terminal::print_xy(0, 0, "Your mom");
-    terminal::with_colors(Color::from_rgb(0xFA, 0xAF, 0x29),
-                          Color::from_rgb(0x05, 0x50, 0xD6),
-                          || terminal::print_xy(0, 1, "Colerd"));
-    for (i, c) in "Coloured letters with pixel-offset!".chars().enumerate() {
-        terminal::put_ext(Point::new(i as i32, 2),
-                          Point::new(i as i32, i as i32),
-                          c,
-                          &vec![Color::from_rgb(0xFF, 0x00, 0x00),
-                                Color::from_rgb(0x00, 0xFF, 0x00),
-                                Color::from_rgb(0x00, 0x00, 0xFF),
-                                Color::from_rgb(0xFF, 0xFF, 0xFF)]);
-    }
-    terminal::refresh();
+    terminal::print_xy(32, 0, "load state A\nand A 0b0001\nJZ SLOT1");
 
-    terminal::set_foreground(Color::from_rgb(0xFF, 0xFF, 0xFF));
-    if let Some(string) = terminal::read_str(Point::new(0, 5), 30) {
-        terminal::print_xy(0, 5, &*&string);
+
+    let size = state::size();
+
+    for y in 0..size.height {
+        terminal::print_xy(0, y, format!("{:#06X}", y).as_ref());
     }
+
+
     terminal::refresh();
     for event in terminal::events() {
         match event {
