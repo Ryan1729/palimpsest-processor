c3cf9d40e4066d67d9ab20916a17aeba95c803b8
Ryan1729 <Ryan1729@gmail.com>
2017-03-20 01:14:27 -0600

    get scrolling working, with the arrow keys
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 extern crate bear_lib_terminal;
 
-use bear_lib_terminal::Color;
-use bear_lib_terminal::geometry::Point;
 use bear_lib_terminal::terminal::{self, config, Event, KeyCode, state};
 
 use std::fmt;
@@ -35,6 +33,7 @@ impl fmt::Display for Data {
 }
 
 #[derive(Debug, Clone, Copy)]
+#[allow(dead_code)]
 enum Register {
     A,
     B,
@@ -53,22 +52,31 @@ impl fmt::Display for Register {
     }
 }
 
+macro_rules! clamp {
+    ( $min : expr, $input: expr, $max: expr ) => {
+
+        if $input < $min {
+            $min
+        } else if $input > $max {
+            $max
+        } else {
+            $input
+        }
+
+    }
+}
 
 fn main() {
     terminal::open("DUEL17", 80, 30);
     terminal::set(config::Window::empty().resizeable(true));
 
-    terminal::print_xy(32, 0, "load state A\nand A 0b0001\nJZ SLOT1");
 
-    let size = state::size();
 
     let instructions = get_instructions();
 
-    for y in 0..size.height {
-        terminal::print_xy(0,
-                           y,
-                           format!("{:#04X}│{}", y, instructions[y as usize]).as_ref());
-    }
+    let mut scroll_offset: i32 = 0;
+
+    draw_instructions(instructions, scroll_offset);
 
     terminal::refresh();
     for event in terminal::events() {
@@ -77,20 +85,71 @@ fn main() {
                 terminal::print_xy(0, 0, &*&format!("Width: {}\nHeight: {}", width, height));
                 terminal::refresh();
             }
+            Event::MouseScroll { delta } => {
+                scroll_offset = scroll_offset.saturating_add(delta);
+            }
+            Event::KeyPressed { key: KeyCode::Up, ctrl: _, shift: _ } => {
+                scroll_offset = clamp_scroll_offset(scroll_offset.saturating_add(-1));
+            }
+            Event::KeyPressed { key: KeyCode::Down, ctrl: _, shift: _ } => {
+                scroll_offset = clamp_scroll_offset(scroll_offset.saturating_add(1));
+            }
             Event::Close |
             Event::KeyPressed { key: KeyCode::Escape, ctrl: _, shift: _ } => break,
             _ => (),
         }
+
+        terminal::clear(None);
+
+        terminal::print_xy(32, 0, "load state A\nand A 0b0001\nJZ SLOT1");
+
+        draw_instructions(instructions, scroll_offset);
+
+        terminal::refresh();
+
     }
     terminal::close();
 }
 
-fn get_instructions() -> [Instruction; 256] {
-    let mut result = [NOP; 256];
+fn clamp_scroll_offset(scroll_offset: i32) -> i32 {
+    let height = state::size().height;
+    let len = PLAYFIELD_SIZE as i32;
+
+    clamp!(-height + 1, scroll_offset, len - 1)
+
+}
+
+const PLAYFIELD_SIZE: usize = 32;
+
+fn draw_instructions(instructions: [Instruction; PLAYFIELD_SIZE], mut scroll_offset: i32) {
+    scroll_offset = clamp_scroll_offset(scroll_offset);
+
+    for y in 0..state::size().height {
+        let address = y + scroll_offset;
+        if let Some(instruction) = instructions.get(address as usize) {
+            terminal::print_xy(0, y, format!("{:#04X}│{}", address, instruction).as_ref());
+
+        } else if address == -1 {
+            terminal::print_xy(0, y, "────┐");
+        } else if address == PLAYFIELD_SIZE as i32 {
+            terminal::print_xy(0, y, "────┘");
+        } else {
+            //don't print anything
+        }
+    }
+}
+
+fn get_instructions() -> [Instruction; PLAYFIELD_SIZE] {
+    let mut result = [NOP; PLAYFIELD_SIZE];
 
     result[2] = Load(State, A);
-    result[5] = Load(State, C);
-    result[8] = Load(State, F);
+    result[4] = Load(State, B);
+    result[8] = Load(State, C);
+    result[16] = Load(State, D);
+    // result[32] = Load(State, E);
+    // result[64] = Load(State, F);
+    // result[128] = Load(State, G);
+    // result[254] = Load(State, H);
 
     result
 }
