887151a6824f276aea6535cbae0d3abd19d0fdcc
Ryan1729 <Ryan1729@gmail.com>
2017-03-21 06:12:55 -0600

    copy from successful reload test based on https://damienradtke.com/post/rusty-dynamic-loading/
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

---
+++ game/src/lib.rs
@@ -0,0 +1,4 @@
+#[no_mangle]
+pub fn get_message() -> &'static str {
+    "load state A\nand A 0b0001\nJZ SLOT1"
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
+extern crate libloading;
 extern crate bear_lib_terminal;
 
 use bear_lib_terminal::terminal::{self, config, Event, KeyCode, state};
+use bear_lib_terminal::geometry::{Point, Rect, Size};
 
 use std::fmt;
+
+
+use libloading::Library;
+
+const LIB_PATH: &'static str = "./target/debug/libgame.so";
+
+struct Application(Library);
+impl Application {
+    fn get_message(&self) -> &'static str {
+        unsafe {
+            let f = self.0.get::<fn() -> &'static str>(b"get_message\0").unwrap();
+            f()
+        }
+    }
+}
 
 #[derive(Clone, Copy)]
 enum Instruction {
@@ -78,20 +95,21 @@ fn main() {
                            both: false,
                        }]);
 
+    let mut app = Application(Library::new(LIB_PATH).unwrap_or_else(|error| panic!("{}", error)));
+
+    let mut last_modified = std::fs::metadata(LIB_PATH).unwrap().modified().unwrap();
+
+    let dur = std::time::Duration::from_secs(1);
 
     let instructions = get_instructions();
 
     let mut scroll_offset: i32 = 0;
 
-    draw_instructions(instructions, scroll_offset);
+    draw(instructions, scroll_offset);
 
     terminal::refresh();
     for event in terminal::events() {
         match event {
-            Event::Resize { width, height } => {
-                terminal::print_xy(0, 0, &*&format!("Width: {}\nHeight: {}", width, height));
-                terminal::refresh();
-            }
             Event::MouseScroll { delta } => {
                 scroll_offset = scroll_offset.saturating_add(delta);
             }
@@ -108,14 +126,61 @@ fn main() {
 
         terminal::clear(None);
 
-        terminal::print_xy(32, 0, "load state A\nand A 0b0001\nJZ SLOT1");
+        terminal::print_xy(32, 0, app.get_message());
 
-        draw_instructions(instructions, scroll_offset);
+        draw(instructions, scroll_offset);
 
         terminal::refresh();
 
+
+
+        if let Ok(Ok(modified)) = std::fs::metadata(LIB_PATH).map(|m| m.modified()) {
+            if modified > last_modified {
+                drop(app);
+                app =
+                    Application(Library::new(LIB_PATH).unwrap_or_else(|error| panic!("{}", error)));
+                last_modified = modified;
+            }
+        }
+
     }
     terminal::close();
+}
+
+fn draw(instructions: [Instruction; PLAYFIELD_SIZE], scroll_offset: i32) {
+    draw_instructions(instructions, scroll_offset);
+
+    let size = state::size();
+
+    draw_rect(12, size.height - 24, 12, 16);
+
+
+}
+
+fn draw_rect(x: i32, y: i32, w: i32, h: i32) {
+    terminal::clear(Some(Rect::from_values(x, y, w, h)));
+
+    let right = x + w;
+    let bottom = y + h;
+    // top
+    terminal::print_xy(x, y, "┌");
+    for i in (x + 1)..right {
+        terminal::print_xy(i, y, "─");
+    }
+    terminal::print_xy(right, y, "┐");
+
+    // sides
+    for i in (y + 1)..bottom {
+        terminal::print_xy(x, i, "│");
+        terminal::print_xy(right, i, "│");
+    }
+
+    //bottom
+    terminal::print_xy(x, bottom, "└");
+    for i in (x + 1)..right {
+        terminal::print_xy(i, bottom, "─");
+    }
+    terminal::print_xy(right, bottom, "┘");
 }
 
 fn clamp_scroll_offset(scroll_offset: i32) -> i32 {
