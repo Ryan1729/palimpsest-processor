ea33a90b71be488300d4397d032e180e66ae23b0
Ryan1729 <Ryan1729@gmail.com>
2017-03-21 20:32:43 -0600

    get back to feature parity with old non-live-reloadable version
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- game/src/lib.rs
+++ game/src/lib.rs
@@ -1,4 +1,109 @@
+extern crate common;
+
+use common::*;
+use common::Register::*;
+use common::Data::*;
+use common::Instruction::*;
+
 #[no_mangle]
 pub fn get_message() -> &'static str {
     "load state A\nand A 0b0001\nJZ SLOT1"
+}
+
+#[no_mangle]
+pub fn draw(platform: &Platform,
+            instructions: [Instruction; common::PLAYFIELD_SIZE],
+            scroll_offset: i32) {
+    draw_instructions(platform, instructions, scroll_offset);
+
+    let size = (platform.size)();
+
+    draw_rect(platform, 12, size.height - 24, 12, 8);
+}
+
+#[no_mangle]
+pub fn draw_rect(platform: &Platform, x: i32, y: i32, w: i32, h: i32) {
+    (platform.clear)(Some(Rect::from_values(x, y, w, h)));
+    //
+    let right = x + w;
+    let bottom = y + h;
+    // top
+    (platform.print_xy)(x, y, "┌");
+    for i in (x + 1)..right {
+        (platform.print_xy)(i, y, "─");
+    }
+    (platform.print_xy)(right, y, "┐");
+
+    // sides
+    for i in (y + 1)..bottom {
+        (platform.print_xy)(x, i, "│");
+        (platform.print_xy)(right, i, "│");
+    }
+
+    //bottom
+    (platform.print_xy)(x, bottom, "└");
+    for i in (x + 1)..right {
+        (platform.print_xy)(i, bottom, "─");
+    }
+    (platform.print_xy)(right, bottom, "┘");
+}
+
+macro_rules! clamp {
+    ( $min : expr, $input: expr, $max: expr ) => {
+
+        if $input < $min {
+            $min
+        } else if $input > $max {
+            $max
+        } else {
+            $input
+        }
+
+    }
+}
+
+#[no_mangle]
+pub fn clamp_scroll_offset(height: i32, scroll_offset: i32) -> i32 {
+    let len = common::PLAYFIELD_SIZE as i32;
+
+    clamp!(-height + 1, scroll_offset, len - 1)
+}
+
+#[no_mangle]
+pub fn draw_instructions(platform: &Platform,
+                         instructions: [Instruction; common::PLAYFIELD_SIZE],
+                         mut scroll_offset: i32) {
+
+    let height = (platform.size)().height;
+    scroll_offset = clamp_scroll_offset(height, scroll_offset);
+
+    for y in 0..height {
+        let address = y + scroll_offset;
+        if let Some(instruction) = instructions.get(address as usize) {
+            (platform.print_xy)(0, y, format!("{:#04X}│{}", address, instruction).as_ref());
+
+        } else if address == -1 {
+            (platform.print_xy)(0, y, "────┐");
+        } else if address == common::PLAYFIELD_SIZE as i32 {
+            (platform.print_xy)(0, y, "────┘");
+        } else {
+            //don't print anything
+        }
+    }
+}
+
+#[no_mangle]
+pub fn get_instructions() -> [Instruction; common::PLAYFIELD_SIZE] {
+    let mut result = [NOP; common::PLAYFIELD_SIZE];
+
+    result[2] = Load(Value, A);
+    result[4] = Load(Value, B);
+    result[8] = Load(Value, C);
+    result[16] = Load(Value, D);
+    // result[32] = Load(Value, E);
+    // result[64] = Load(Value, F);
+    // result[128] = Load(Value, G);
+    // result[254] = Load(Value, H);
+
+    result
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use libloading::Library;
 use bear_lib_terminal::terminal::{self, config, Event, KeyCode, state};
 use bear_lib_terminal::geometry::{Point, Rect, Size};
 
+use std::mem;
+
 use common::*;
 use common::Register::*;
 use common::Data::*;
@@ -22,19 +24,30 @@ impl Application {
             f()
         }
     }
-}
-
-macro_rules! clamp {
-    ( $min : expr, $input: expr, $max: expr ) => {
-
-        if $input < $min {
-            $min
-        } else if $input > $max {
-            $max
-        } else {
-            $input
+    fn clamp_scroll_offset(&self, scroll_offset: i32) -> i32 {
+        unsafe {
+            let f = self.0.get::<fn(i32) -> i32>(b"clamp_scroll_offset\0").unwrap();
+            f(scroll_offset)
         }
-
+    }
+    fn get_instructions(&self) -> [Instruction; common::PLAYFIELD_SIZE] {
+        unsafe {
+            let f = self.0
+                .get::<fn() -> [Instruction; common::PLAYFIELD_SIZE]>(b"get_instructions\0")
+                .unwrap();
+            f()
+        }
+    }
+    fn draw(&self,
+            platform: &Platform,
+            instructions: [Instruction; common::PLAYFIELD_SIZE],
+            scroll_offset: i32) {
+        unsafe {
+            let f = self.0
+                .get::<fn(&Platform, [Instruction; common::PLAYFIELD_SIZE], i32)>(b"draw\0")
+                .unwrap();
+            f(platform, instructions, scroll_offset)
+        }
     }
 }
 
@@ -50,17 +63,23 @@ fn main() {
                            both: false,
                        }]);
 
+
+
     let mut app = Application(Library::new(LIB_PATH).unwrap_or_else(|error| panic!("{}", error)));
 
     let mut last_modified = std::fs::metadata(LIB_PATH).unwrap().modified().unwrap();
 
-    let dur = std::time::Duration::from_secs(1);
-
-    let instructions = get_instructions();
+    let instructions = app.get_instructions();
 
     let mut scroll_offset: i32 = 0;
 
-    draw(instructions, scroll_offset);
+    let platform = Platform {
+        print_xy: terminal::print_xy,
+        clear: clear,
+        size: size,
+    };
+
+    app.draw(&platform, instructions, scroll_offset);
 
     terminal::refresh();
     for event in terminal::events() {
@@ -69,10 +88,10 @@ fn main() {
                 scroll_offset = scroll_offset.saturating_add(delta);
             }
             Event::KeyPressed { key: KeyCode::Up, ctrl: _, shift: _ } => {
-                scroll_offset = clamp_scroll_offset(scroll_offset.saturating_add(-1));
+                scroll_offset = app.clamp_scroll_offset(scroll_offset.saturating_add(-1));
             }
             Event::KeyPressed { key: KeyCode::Down, ctrl: _, shift: _ } => {
-                scroll_offset = clamp_scroll_offset(scroll_offset.saturating_add(1));
+                scroll_offset = app.clamp_scroll_offset(scroll_offset.saturating_add(1));
             }
             Event::Close |
             Event::KeyPressed { key: KeyCode::Escape, ctrl: _, shift: _ } => break,
@@ -83,7 +102,7 @@ fn main() {
 
         terminal::print_xy(32, 0, app.get_message());
 
-        draw(instructions, scroll_offset);
+        app.draw(&platform, instructions, scroll_offset);
 
         terminal::refresh();
 
@@ -102,81 +121,10 @@ fn main() {
     terminal::close();
 }
 
-fn draw(instructions: [Instruction; PLAYFIELD_SIZE], scroll_offset: i32) {
-    draw_instructions(instructions, scroll_offset);
-
-    let size = state::size();
-
-    draw_rect(12, size.height - 24, 12, 16);
-
-
+fn clear(rect: Option<common::Rect>) {
+    unsafe { terminal::clear(mem::transmute::<Option<common::Rect>, Option<Rect>>(rect)) };
 }
 
-fn draw_rect(x: i32, y: i32, w: i32, h: i32) {
-    terminal::clear(Some(Rect::from_values(x, y, w, h)));
-
-    let right = x + w;
-    let bottom = y + h;
-    // top
-    terminal::print_xy(x, y, "┌");
-    for i in (x + 1)..right {
-        terminal::print_xy(i, y, "─");
-    }
-    terminal::print_xy(right, y, "┐");
-
-    // sides
-    for i in (y + 1)..bottom {
-        terminal::print_xy(x, i, "│");
-        terminal::print_xy(right, i, "│");
-    }
-
-    //bottom
-    terminal::print_xy(x, bottom, "└");
-    for i in (x + 1)..right {
-        terminal::print_xy(i, bottom, "─");
-    }
-    terminal::print_xy(right, bottom, "┘");
-}
-
-fn clamp_scroll_offset(scroll_offset: i32) -> i32 {
-    let height = state::size().height;
-    let len = PLAYFIELD_SIZE as i32;
-
-    clamp!(-height + 1, scroll_offset, len - 1)
-
-}
-
-const PLAYFIELD_SIZE: usize = 32;
-
-fn draw_instructions(instructions: [Instruction; PLAYFIELD_SIZE], mut scroll_offset: i32) {
-    scroll_offset = clamp_scroll_offset(scroll_offset);
-
-    for y in 0..state::size().height {
-        let address = y + scroll_offset;
-        if let Some(instruction) = instructions.get(address as usize) {
-            terminal::print_xy(0, y, format!("{:#04X}│{}", address, instruction).as_ref());
-
-        } else if address == -1 {
-            terminal::print_xy(0, y, "────┐");
-        } else if address == PLAYFIELD_SIZE as i32 {
-            terminal::print_xy(0, y, "────┘");
-        } else {
-            //don't print anything
-        }
-    }
-}
-
-fn get_instructions() -> [Instruction; PLAYFIELD_SIZE] {
-    let mut result = [NOP; PLAYFIELD_SIZE];
-
-    result[2] = Load(Value, A);
-    result[4] = Load(Value, B);
-    result[8] = Load(Value, C);
-    result[16] = Load(Value, D);
-    // result[32] = Load(Value, E);
-    // result[64] = Load(Value, F);
-    // result[128] = Load(Value, G);
-    // result[254] = Load(Value, H);
-
-    result
+fn size() -> common::Size {
+    unsafe { mem::transmute::<Size, common::Size>(state::size()) }
 }
